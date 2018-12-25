f88d72b9cced76af0bcd06c7b25fb7a2ea0bbf30
Denton <denton35@outlook.com>
2018-12-25 23:14:13 +0100

    Complete Code

--- LED/LED.ino
+++ LED/LED.ino
@@ -1,15 +1,38 @@
 int lowIn = A0;
 int midianIn = A1;
 int highIn = A2;
+int level = 0;  // 0 -> Low, 1 -> Median, 2 -> High
+int currentLevel = 0;
+int resetHighPin = 3;
+int resetMedianPin = 2;
+int resetValue = 4;
+int lowLEDPin = 9;
+int medianLEDPin = 10;
+int highLEDPin = 11; 
+
+boolean isChanged = false;
+boolean isRest = false;
 
 void setup() {
   Serial.begin(9600);   //Set the baud rate of the comunication
   pinMode(lowIn,INPUT);    //Define the pin as input
   pinMode(midianIn,INPUT);
   pinMode(highIn,INPUT);
+  
+  pinMode(resetMedianPin, INPUT_PULLUP);
+  pinMode(resetHighPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(resetMedianPin), resetMedian, RISING);
+  attachInterrupt(digitalPinToInterrupt(resetHighPin), resetHigh, RISING);
+
+  pinMode(lowLEDPin, OUTPUT); 
+  pinMode(medianLEDPin, OUTPUT);
+  pinMode(highLEDPin, OUTPUT);
+  
+  Serial.print(resetValue);  //default notification
 }
 
 void loop() {
+  delay(1000);//Small delay between each data send 
   //Read the analog value
   float lowVal = analogRead(lowIn);
   float midianVal = analogRead(midianIn);
@@ -19,10 +42,69 @@ void loop() {
   float lowVolt = lowVal/205;
   float midianVolt = midianVal/205;
   float highVolt = highVal/205;
+  if(lowVolt < 0.5) {
+    currentLevel = 0;
+    if(currentLevel != level){
+      isChanged = true;
+      level = currentLevel;
+    }
+  }
   
-  //Use serial.print to send the data in a "text" format
-  //Serial.println(lowVolt);
-  //Serial.println(midianVolt);
-  Serial.println(highVolt);
-  delay(50);//Small delay between each data send 
+  if(midianVolt < 0.5) {
+    currentLevel = 1;
+    if(currentLevel != level){
+      isChanged = true;
+      level = currentLevel;
+    }
+  }
+  
+  if(highVolt < 0.5) {
+   currentLevel = 2;
+    if(currentLevel != level){
+      isChanged = true;
+      level = currentLevel;
+    }
+  }
+  
+  
+  if(isChanged && !isRest){
+    Serial.print(level);
+    isChanged = false;
+  }
+
+  if(isRest){
+    Serial.print(resetValue);
+    isRest = false;
+  }
+
+  if(level == 0){
+    digitalWrite(lowLEDPin, HIGH);
+    digitalWrite(medianLEDPin, LOW);
+    digitalWrite(highLEDPin, LOW);
+  }
+  
+  if(level == 1){
+    digitalWrite(lowLEDPin, HIGH);
+    digitalWrite(medianLEDPin, HIGH);
+    digitalWrite(highLEDPin, LOW);
+  }
+  
+  if(level == 2){
+    digitalWrite(lowLEDPin, HIGH);
+    digitalWrite(medianLEDPin, HIGH);
+    digitalWrite(highLEDPin, HIGH);
+  }
+
+}
+
+void resetMedian(){
+  level = 1;
+  resetValue = 5;
+  isRest = true;
+}
+
+void resetHigh(){
+  level = 2;
+  resetValue = 6;
+  isRest = true;
 }
